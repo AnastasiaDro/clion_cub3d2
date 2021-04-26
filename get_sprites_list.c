69f65773ff5cba97c962347848fb88c3575ee9de
cerebus <cerebus@student.21-school.ru>
2021-04-26 14:24:21 +0300

    -normed get_sprites_list.c

--- get_sprites_list.c
+++ get_sprites_list.c
@@ -1,13 +1,22 @@
-//
-// Created by  Anastasia on 25.04.2021.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_sprites_list.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/26 14:22:03 by cerebus           #+#    #+#             */
+/*   Updated: 2021/04/26 14:23:09 by cerebus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub_utils.h"
 
-int get_sprites_list(char *s, t_data *m_struct, int elems_num)
+int	get_sprites_list(char *s, t_data *m_struct, int elems_num)
 {
-	int i;
-	double x;
-	double y;
+	int		i;
+	double	x;
+	double	y;
 
 	i = 0;
 	while (s[i])
@@ -16,10 +25,11 @@ int get_sprites_list(char *s, t_data *m_struct, int elems_num)
 		{
 			x = i + 0.5;
 			y = elems_num - 1 + 0.5;
-			sprite_lstadd_front(m_struct->sprite_info->sprite_lst, sprite_lstnew(x, y));
+			sprite_lstadd_front(m_struct->sprite_info->sprite_lst, \
+									sprite_lstnew(x, y));
 			m_struct->sprite_info->num_sprites += 1;
 		}
 		i++;
 	}
-	return i;
+	return (i);
 }
