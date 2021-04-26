a0e2fcf59b040e785474f040fbdfb05d7d8b1d6c
cerebus <adrogunova@gmail.com>
2021-04-27 01:55:41 +0300

    -normed sortSprites.h!!!!

--- sortSprites.h
+++ sortSprites.h
@@ -1,30 +1,37 @@
-//
-// Created by Cesar Erebus on 4/27/21.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sortSprites.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 01:54:03 by cerebus           #+#    #+#             */
+/*   Updated: 2021/04/27 01:55:33 by cerebus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef SORTSPRITES_H
 # define SORTSPRITES_H
-
-
-#include "cub_utils.h"
-#include "sprites_utils.h"
-#include "draw_sprites.h"
+# include "cub_utils.h"
+# include "sprites_utils.h"
+# include "draw_sprites.h"
 
 typedef struct s_sort_spr {
-        int found;
-        double sec_distance;
-        double tmp_distance;
-        double sec_x;
-        double sec_y;
-        double tmp_x;
-        double tmp_y;
-}              t_sort_spr;
+	int		found;
+	double	sec_distance;
+	double	tmp_distance;
+	double	sec_x;
+	double	sec_y;
+	double	tmp_x;
+	double	tmp_y;
+}				t_sort_spr;
 
-void		sortSprites(t_sprite **sprite_list);
+void	sortSprites(t_sprite **sprite_list);
 
-void move(t_sprite **t1, t_sprite **t2, t_sort_spr *t_sort);
+void	move(t_sprite **t1, t_sprite **t2, t_sort_spr *t_sort);
 
-void parse_t2_smaller(t_sprite **t1, t_sprite **t2, t_sort_spr *t_sort);
+void	parse_t2_smaller(t_sprite **t1, t_sprite **t2, t_sort_spr *t_sort);
 
-void  parse_t2_biger(t_sprite **t1, t_sprite **t2, t_sort_spr *t_sort);
+void	parse_t2_biger(t_sprite **t1, t_sprite **t2, t_sort_spr *t_sort);
+
 #endif
