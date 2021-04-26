2728802d605d4109da4d44b37dd960a12d1a1a20
cerebus <adrogunova@gmail.com>
2021-04-26 18:45:45 +0300

    -made more norm in ray.*

--- ray.h
+++ ray.h
@@ -1,45 +1,41 @@
-//
-// Created by  Anastasia on 25.03.2021.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/26 18:40:13 by cerebus           #+#    #+#             */
+/*   Updated: 2021/04/26 18:41:38 by cerebus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef RAY_H
-#define RAY_H
-#include "cub_utils.h"
+# define RAY_H
+# include "cub_utils.h"
 typedef struct s_ray
 {
-    //вектор направления луча
-    double DirX;
-    double DirY;
+	double	DirX;
+	double	DirY;
+	double	sideDistX;
+	double	sideDistY;
+	double	deltaDistX;
+	double	deltaDistY;
+	double	cameraX;
+	int		hit;
+	int		stepX;
+	int		stepY;
+	int		mapX;
+	int		mapY;
+	double	perpWallDist;
+}				t_ray;
 
-    //length of ray from current position to next x or y-side
-    double sideDistX;
-    double sideDistY;
+void	calc_ray_dir(t_ray *ray, t_data *m_struct, int x);
 
-    //length of ray from one x or y-side to next x or y-side
-    double deltaDistX;
-    double deltaDistY;
+void	calc_ray_length(t_ray *ray);
 
-    //точка на векторе камеры, счиается за 100% ширина экрана
-    double cameraX;
+void	calc_step_side_dist(t_ray *ray, t_data *m_struct);
 
-    int hit;
+void	set_ray_params(t_ray *ray, t_data *m_struct, int x);
 
-    int stepX;
-    int stepY;
-
-    int mapX;
-    int mapY;
-
-    //расстояние до стены?
-    double perpWallDist;
-}               t_ray;
-
-void calc_ray_dir(t_ray *ray, t_data *m_struct, int x);
-
-void calc_ray_length(t_ray *ray);
-
-void calc_step_side_dist(t_ray *ray, t_data *m_struct);
-
-void set_ray_params(t_ray *ray, t_data *m_struct, int x);
-
-#endif //CLION_CUB3D_RAY_H
+#endif
