76504e37fd3fade83c34e97477fc4638e3d3014f
cerebus <cerebus@student.21-school.ru>
2021-04-26 14:21:46 +0300

    -normed fill_map.*

--- fill_map.c
+++ fill_map.c
@@ -1,12 +1,21 @@
-//
-// Created by  Anastasia on 25.04.2021.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fill_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/26 14:15:06 by cerebus           #+#    #+#             */
+/*   Updated: 2021/04/26 14:19:55 by cerebus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <printf.h>
 #include "cub_utils.h"
 #include "parser_utils.h"
 #include "exceptions.h"
 
-void check_was_player(int flag_player, t_data *m_struct)
+void	check_was_player(int flag_player, t_data *m_struct)
 {
 	if (flag_player == 0)
 	{
@@ -16,44 +25,45 @@ void check_was_player(int flag_player, t_data *m_struct)
 	}
 }
 
-void set_player_coords(t_data *m_struct, int is_player, int elems_num, char **map)
+void	set_player_coords(t_data *m_struct, \
+			int is_player, int elems_num, char **map)
 {
 	m_struct->map_player_x = (double)is_player + 0.5;
 	m_struct->map_player_y = (double)(elems_num - 1) + 0.5;
 	map[elems_num - 1][is_player] = '0';
 }
 
-void find_player(int *elems_num, t_list **elem, t_data *m_struct, char **map)
+void	find_player(int *elems_num, t_list **elem, t_data *m_struct, char **map)
 {
 	int is_player;
-	int flag_player;
+	int flag;
 
-	flag_player = 0;
+	flag = 0;
 	while (*elems_num)
 	{
 		map[(*elems_num) - 1] = (char *)(*elem)->content;
 		(*elem)->content = NULL;
-		is_player = check_symbols(map[(*elems_num)-1], m_struct, &flag_player);
-		if(is_player)
+		is_player = check_symbols(map[(*elems_num) - 1], m_struct, &flag);
+		if (is_player)
 			set_player_coords(m_struct, is_player, *elems_num, map);
 		get_sprites_list(map[(*elems_num) - 1], m_struct, *elems_num);
 		(*elem) = (*elem)->next;
 		(*elems_num)--;
 	}
-	check_was_player(flag_player, m_struct);
+	check_was_player(flag, m_struct);
 }
 
-int fill_map(t_list **last_elem, int elems_num, t_data *m_struct)
+int		fill_map(t_list **last_elem, int elems_num, t_data *m_struct)
 {
-	char **map;
-	t_list *elem;
-	char *st;
-	int el_num;
+	char	**map;
+	t_list	*elem;
+	char	*st;
+	int		el_num;
 
-	map = ft_calloc(elems_num +1 , sizeof(char *)); //количество наших строчек + 1 для зануления
+	map = ft_calloc(elems_num + 1, sizeof(char *));
 	elem = *last_elem;
 	st = (char *)elem->content;
-	while('\0' == *st)
+	while ('\0' == *st)
 	{
 		elem = elem->next;
 		elems_num--;
