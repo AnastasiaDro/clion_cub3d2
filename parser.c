dd4bf5b299cee8380d61d28dad2136516da15025
cerebus <adrogunova@gmail.com>
2021-04-27 04:16:11 +0300

    -added end_file_parsing

--- parser.c
+++ parser.c
@@ -42,6 +42,38 @@ void	check_last_line(t_data *m_struct, \
 	}
 }
 
+
+
+int cmp_space_str(char *line)
+{
+    int slen;
+    char *cmp_str;
+    int res;
+
+    if (*line == '\0')
+        return (0);
+    slen = (int)ft_strlen(line);
+    cmp_str = malloc((slen + 1) * sizeof(char));
+    while(slen >= 0)
+    {
+        cmp_str[slen-1] = ' ';
+        slen--;
+    }
+    res = ft_strncmp(line, cmp_str, slen);
+    free(cmp_str);
+    return (res);
+}
+
+void check_file_end(char *line, t_data *m_struct)
+{
+    if (!cmp_space_str(line))
+    {
+        free(line);
+        throw_map_except(m_struct);
+        exit(0);
+    }
+}
+
 void	parse_map(t_data *m_struct, char *map_path)
 {
 	int		fd;
@@ -57,6 +89,12 @@ void	parse_map(t_data *m_struct, char *map_path)
 		ft_lstadd_front(&last_elem, ft_lstnew(line));
 		elems_num++;
 	}
+    check_file_end(line, m_struct);
+//    if (!cmp_space_str(line))
+//    {
+//        throw_map_except(m_struct);
+//        exit(0);
+//    }
 	ft_lstadd_front(&last_elem, ft_lstnew(line));
 	elems_num++;
 	m_struct->lst = last_elem;
