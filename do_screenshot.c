afc180543d8d6dbb622c6502bf064e43480e12b4
cerebus <adrogunova@gmail.com>
2021-04-27 22:02:51 +0300

    - made bmp

--- do_screenshot.c
+++ do_screenshot.c
@@ -18,32 +18,14 @@
 
 int fill_shot(t_data *m_struct, int fd)
 {
-//    int x;
-//    int y;
-//    int color;
-//    y = m_struct->params->screen_h;
-
     write(fd, m_struct->addr, m_struct->line_length * m_struct->params->screen_h);
-
-//    while (y > 0)
-//    {
-//        x = 0;
-//        while (x < m_struct->params->screen_h)
-//         {
-//            color = *(int *)(m_struct->addr + (m_struct->line_length * y + \
-//            x * (m_struct->bits_per_pixel / 8)));
-//             write(fd, &color, 4);
-//            x++;
-//        }
-//        y--;
-//    }
     return (1);
 }
 
 static void fill_header(t_data *m_struct,  t_scr_shot *shot, int *fd)
 {
     *fd = open("cub3D.bmp", O_CREAT | O_RDWR | O_TRUNC, 0777);
-   shot->header[0] = 'B';
+    shot->header[0] = 'B';
     shot->header[1] = 'M';
     shot->header[2] = (unsigned char)shot->size;
     shot->header[3] = (unsigned char)(shot->size >> 8);
@@ -70,29 +52,17 @@ void save(t_data *m_struct)
 {
     t_scr_shot shot;
     t_scr_shot *p_shot;
-    int i;
     int fd;
 
-    i = 0;
     fd = 0;
     p_shot = &shot;
     shot.size = 54 + (m_struct->bits_per_pixel / 8 * m_struct->params->screen_w * m_struct->params->screen_h);
-//    all->save.size = 54 + (all->ptr.bits_per_pixel / 8 * \
-//    all->map.r_width * all->map.r_height);
-    shot.header = malloc(54 * sizeof(unsigned char));
-    //all->save.header = ft_calloc(54, sizeof(char));
+    shot.header = ft_calloc(54, sizeof (unsigned char));
      if (!shot.header)
      {
          throw_exception(MALLOC_ERROR);
          free_all(m_struct);
      }
-     //?
-     while (i < 54)
-    {
-         p_shot->header[i] = (unsigned char)(0);
-         i++;
-    }
-
     fill_header(m_struct, p_shot, &fd);
     fill_shot(m_struct, fd);
     free_all(m_struct);
